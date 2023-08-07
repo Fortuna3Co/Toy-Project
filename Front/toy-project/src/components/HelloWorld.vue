34a513c5d4b17685e16a0f4ebb4dd638508caff7
fortuna3co <fortuna3co@gmail.com>
2023-08-07 21:15:02 +0900

    초기 환경설정 구성 완료  Back : Java17, SpringBoot  Front : Vue3, Vuetify  Front  Vuetify layout 적용

---
+++ Front/toy-project/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="Application bar">
+      <div class="d-flex justify-end mb-6 bg-surface-variant">
+      <v-sheet
+        v-for="n in 3"
+        :key="n"
+        class="ma-2 pa-2"
+      >
+        justify-end
+      </v-sheet>
+    </div>
+    </v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      Main Content
+    </v-main>
+  </v-layout>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+  }),
+}
+</script>
