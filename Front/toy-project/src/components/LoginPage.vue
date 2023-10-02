19b53da8f4e61d7670a460280191b47ad0b4b458
fortuna3co <fortuna3co@gmail.com>
2023-10-02 16:44:09 +0900

    HelloWorlde.vue : 로그인 아이콘 생성, LoginPage.vue : 로그인 페이지 화면 생성, SignUpPage.vue : 회원가입 페이지 화면 생성, index.js : login, signUp 경로 생성

---
+++ Front/toy-project/src/components/LoginPage.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-sheet class="pa-12" rounded>
+        <v-card class="mx-auto px-6 py-8" max-width="60%" :elevation="12">
+            <v-form
+            v-model="form"
+            @submit.prevent="onSubmit"
+            >
+                <v-text-field
+                    v-model="email"
+                    :readonly="loading"
+                    :rules="[required]"
+                    class="mb-2"
+                    clearable
+                    label="Email"
+                ></v-text-field>
+        
+                <v-text-field
+                    v-model="password"
+                    :readonly="loading"
+                    :rules="[required]"
+                    clearable
+                    label="Password"
+                    type="password"
+                    placeholder="Enter your password"
+                ></v-text-field>
+        
+                <br>
+        
+                <v-btn
+                    :disabled="!form"
+                    :loading="loading"
+                    block
+                    color="success"
+                    size="large"
+                    type="submit"
+                    variant="elevated"
+                >
+                    Sign In
+                </v-btn>
+
+                <br>
+                
+                <v-btn
+                    :loading="loading"
+                    block
+                    color="primary"
+                    size="large"
+                    type="submit"
+                    variant="elevated"
+                    @click="$router.push('/signUp')"
+                >
+                    Sign Up
+                </v-btn>
+            </v-form>
+        </v-card>
+    </v-sheet>
+</template>
+
+<script>
+
+</script>
--- Front/toy-project/src/router/index.js
+++ Front/toy-project/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginPage from '../components/LoginPage.vue'
+import SignUpPage from '../components/SignUpPage.vue'
 
 const routes = [
   {
@@ -14,6 +16,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginPage
+  },
+  {
+    path: '/signUp',
+    name: 'signUp',
+    component: SignUpPage
   }
 ]
 
